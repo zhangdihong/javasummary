8c96be81cdafd5748d4b7bbfc8acee3de38b7ccb
zhangdihong <572083747@qq.com>
2015-09-07 00:26:40 +0800

    copy someone  email function

--- ZDHCore/src/main/java/com/zhuanleme/util/EmailUtil.java
+++ ZDHCore/src/main/java/com/zhuanleme/util/EmailUtil.java
@@ -1,11 +1,10 @@
 package com.zhuanleme.util;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.*;
 import java.io.*;
 import java.net.URL;
 import java.util.Properties;
@@ -18,7 +17,6 @@ import java.util.Properties;
  * <p/>
  * <p>Copyright: Copyright (c) 2015 </p>
  * <p/>
- * <p>Company: 华炜云商科技有限公司 www.hwtech.cc</p>
  *
  * @author zhangdihong
  * @version 1.0
@@ -89,7 +87,7 @@ public class EmailUtil {
 
             String content = sendHtml.toString();
             // 邮件内容,也可以使纯文本 "text/plain"
-            mimeMessage.setContent(content, "text/html;charset=UTF-8");
+            mimeMessage.setContent(content, "text/plain;charset=UTF-8");
 
             // 保存邮件
             mimeMessage.saveChanges();
@@ -102,6 +100,75 @@ public class EmailUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }  finally {
+            if (transport != null) {
+                try {
+                    transport.close();
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    /**
+     * 发送邮件
+     *
+     * @param subject     邮件主题
+     * @param sendHtml    邮件内容
+     * @param receiveUser 收件人地址
+     * @param attachment  附件
+     */
+    public void doSendHtmlEmail(String subject, String sendHtml, String receiveUser, File attachment) {
+        try {
+            // 发件人
+            InternetAddress from = new InternetAddress(sender_username);
+            mimeMessage.setFrom(from);
+
+            // 收件人
+            InternetAddress to = new InternetAddress(receiveUser);
+            mimeMessage.setRecipient(Message.RecipientType.TO, to);
+
+            // 邮件主题
+            mimeMessage.setSubject(subject);
+
+            // 向multipart对象中添加邮件的各个部分内容，包括文本内容和附件
+            Multipart multipart = new MimeMultipart();
+
+            // 添加邮件正文
+            BodyPart contentPart = new MimeBodyPart();
+            contentPart.setContent(sendHtml, "text/html;charset=UTF-8");
+            multipart.addBodyPart(contentPart);
+
+            // 添加附件的内容
+            if (attachment != null) {
+                BodyPart attachmentBodyPart = new MimeBodyPart();
+                DataSource source = new FileDataSource(attachment);
+                attachmentBodyPart.setDataHandler(new DataHandler(source));
+
+                // 网上流传的解决文件名乱码的方法，其实用MimeUtility.encodeWord就可以很方便的搞定
+                // 这里很重要，通过下面的Base64编码的转换可以保证你的中文附件标题名在发送时不会变成乱码
+                //sun.misc.BASE64Encoder enc = new sun.misc.BASE64Encoder();
+                //messageBodyPart.setFileName("=?GBK?B?" + enc.encode(attachment.getName().getBytes()) + "?=");
+
+                //MimeUtility.encodeWord可以避免文件名乱码
+                attachmentBodyPart.setFileName(MimeUtility.encodeWord(attachment.getName()));
+                multipart.addBodyPart(attachmentBodyPart);
+            }
+
+            // 将multipart对象放到message中
+            mimeMessage.setContent(multipart);
+            // 保存邮件
+            mimeMessage.saveChanges();
+
+            transport = session.getTransport("smtp");
+            // smtp验证，就是你用来发邮件的邮箱用户名密码
+            transport.connect(mailHost, port, sender_username, sender_password);
+            // 发送
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+
+            System.out.println("send success!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             if (transport != null) {
                 try {
                     transport.close();
